e5dae57e367d643a9bd6acae84a3cb18cb8f7106
Jashnavi7 <jangilijashnavi764@gmail.com>
2026-07-28 13:37:27 +0530

    Tokens

---
+++ Day1.py
@@ -0,0 +1,22 @@
+print("Hello Codegnan")
+
+name = 'Jashnavi'
+print(name)
+email_id = 'jangilijashnavi764@gmail.com'
+print(email_id)
+#gets error   special characters cant be used at starting , cant start with numbers ,cant use keywords  
+'''@roll_no = 22
+print('@roll_no')'''
+
+
+'''TOKENS:  keywords = reserved words
+variable = named memory location
+literals = constants
+operators = perform operations -- 7 types'''
+
+
+#Multiassigment of variables
+name,location,age = 'Jashnavi','Hyd',22
+print(name)
+print(age)
+print(location)
